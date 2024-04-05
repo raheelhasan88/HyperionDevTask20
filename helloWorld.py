2bf8185fb5c7f897fc4569afbc283bbc62f2304a
Raheel Hasan <raheel.hasan88@gmail.com>
2024-04-05 17:04:34 +0100

    Initial commit

---
+++ helloWorld.py
@@ -0,0 +1,5 @@
+# first requesting the user to provide an input as per requirements of Practical Task 2 
+msg = input ("Please provide a message to print: ")
+
+# printing the message as per Practical Task 2 initial instructions
+print (f"User has inputted the message : {msg}")
