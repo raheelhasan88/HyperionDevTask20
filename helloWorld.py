9af022ad9261aaaf46629ce71fa6153f35afeeb6
Raheel Hasan <raheel.hasan88@gmail.com>
2024-04-05 17:09:45 +0100

    Added date and time feature

--- helloWorld.py
+++ helloWorld.py
@@ -1,5 +1,19 @@
+# importing relevant libraries to be able to print date/time
+import os
+from datetime import datetime, date
+
+# defining a function which prints out the current date and time
+
+def print_current_date_and_time():
+    print (f"Today's date and time is:  {datetime.today()}")
+
 # first requesting the user to provide an input as per requirements of Practical Task 2 
+
 msg = input ("Please provide a message to print: ")
 
 # printing the message as per Practical Task 2 initial instructions
 print (f"User has inputted the message : {msg}")
+
+# call function to print date and time
+print_current_date_and_time()
+
